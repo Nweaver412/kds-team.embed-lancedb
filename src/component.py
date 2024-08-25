1b4013e8a6513793945279f0d56b35c5693e1f08
Nweaver412 <nw21010@gmail.com>
2024-08-24 18:49:56 -0600

    push with more dependencies

--- src/component.py
+++ src/component.py
@@ -4,11 +4,32 @@ import os
 import shutil
 import zipfile
 import lancedb
+import asyncio
+import csv
+import dataclasses
+import logging
+from typing import List
+import json
+import os
+
+from io import StringIO
+from itertools import islice
+import pystache as pystache
+import requests.exceptions
+
+from keboola.component.base import ComponentBase, sync_action
+from keboola.component.sync_actions import ValidationResult, MessageType
+from keboola.component.dao import TableDefinition
+from keboola.component.exceptions import UserException
+
+from kbcstorage.tables import Tables
+from kbcstorage.client import Client
+
 import pyarrow as pa
 import pandas as pd
-from keboola.component.base import ComponentBase
-from keboola.component.exceptions import UserException
+
 from configuration import Configuration
+
 from openai import OpenAI
 class Component(ComponentBase):
     def __init__(self):

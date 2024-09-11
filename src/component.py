2218cede6b01fd433a3dd477f23c8d8ffe810ec2
Nweaver412 <nw21010@gmail.com>
2024-09-11 17:39:48 -0600

    small changes

--- src/component.py
+++ src/component.py
@@ -31,7 +31,7 @@ class Component(ComponentBase):
                     os.makedirs(lance_dir, exist_ok=True)
                     db = lancedb.connect(lance_dir)
                     schema = self._get_lance_schema(reader.fieldnames)
-                    table = db.create_table(self._configuration.destination.output_table_name, schema=schema, mode="overwrite")
+                    table = db.create_table("embeddings", schema=schema, mode="overwrite")
                 elif self._configuration.outputFormat == 'csv':
                     output_table = self._get_output_table()
                     output_file = open(output_table.full_path, 'w', encoding='utf-8', newline='')
